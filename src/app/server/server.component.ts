af36aaea94c452e7ec1f0d43448f44d5e831df18
Fernando <fernando.borges@safe.com>
2022-08-08 09:39:22 -0700

    App Server added

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+//add decorator
+@Component({
+    selector: 'app-server', 
+    templateUrl: './server.component.html'
+})
+
+//export component as class
+export class ServerComponent {
+
+}
